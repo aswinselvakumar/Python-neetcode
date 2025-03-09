f3d475f6964d41126bdc2fc5f249e09fcc8c66e6
Aswin S <aswinselvakumar4889@gmail.com>
2025-03-09 15:51:29 +0530

    initial commit

--- multiple_assignment.py
+++ multiple_assignment.py
@@ -1,4 +1,5 @@
 msg1,msg2 = "World" ,"Hello"
 msg1,msg2 = msg2,msg1
 #swaping msg1 & msg2
+#this is genius doing swap in the same line avoids the error
 print(msg1,msg2)
---
+++ variable_types.py
@@ -0,0 +1,11 @@
+age = 20
+temp = 98.6
+is_true = True
+name = "Aswin"
+my_list=[1,2,3]
+
+print(type(age))
+print(type(temp))
+print(type(is_true))
+print(type(name))
+print(type(my_list))
