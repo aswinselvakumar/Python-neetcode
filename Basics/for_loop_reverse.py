1d0ab7d44cf0c003ccb255cdf38df6556151009c
Aswin S <aswinselvakumar4889@gmail.com>
2025-03-10 21:13:37 +0530

    looping statements

---
+++ Basics/for_loop_reverse.py
@@ -0,0 +1,8 @@
+#for loop has "reversed"
+#lets say we want to reverse 0->10
+
+for i in reversed(range(0,11)):
+    print(i)
+
+for i in reversed(range(0,5,2)):
+    print(i)
---
+++ Basics/for_loops4.py
@@ -0,0 +1,5 @@
+#adding increments in the for loops
+for i in range(0,11,2):
+    print(i)
+
+#prints all the even numbers
