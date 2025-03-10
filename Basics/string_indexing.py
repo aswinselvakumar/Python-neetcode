48c657ece335a9b428f5fcb0a3d09dd1a02a369f
Aswin S <aswinselvakumar4889@gmail.com>
2025-03-10 21:47:42 +0530

    string indexing

---
+++ Basics/string_indexing.py
@@ -0,0 +1,5 @@
+#index is always zero based
+
+my_str = "Hello"
+print(my_str[0])
+print(my_str[1])
---
+++ Basics/string_indexing2.py
@@ -0,0 +1,10 @@
+def firstchar(word:str)->None:
+    print(word[0])
+def secondchar(word:str)->None:
+    print(word[1])
+def lastchar(word:str)->None:
+    print(word[len(word)-1])
+
+firstchar("Hello")
+secondchar("Aswin")
+lastchar("Luke")
