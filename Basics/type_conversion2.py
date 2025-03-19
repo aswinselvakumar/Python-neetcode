a86fab3879e945f5307a6c4bf789cec5b95b20b3
Aswin S <aswinselvakumar4889@gmail.com>
2025-03-19 21:26:25 +0530

    type conversion commit

---
+++ Basics/type_conversion2.py
@@ -0,0 +1,10 @@
+def read_integer():
+    user_input = int(input("Enter only integer values :"))
+    return user_input
+
+def read_float():
+    user_input = float(input("Enter only float values :"))
+    return user_input
+
+read_integer()
+read_float()
